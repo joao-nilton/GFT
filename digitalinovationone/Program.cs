1ef4318edcf8cd920f9e64fbce9d567b4f3985cc
joao <joao.nilton@gmail.com>
2021-02-04 16:24:59 -0300

    ver_2_040221

--- digitalinovationone/Program.cs
+++ digitalinovationone/Program.cs
@@ -6,7 +6,12 @@ namespace digitalinovationone
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            int numeroDeVezes = 5;
+            for(int i=0; i< numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem vindo ao curso de .NET {i}");
+            }
         }
     }
 }

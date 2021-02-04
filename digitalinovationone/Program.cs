b4d13e1750db0f548baf1dd570248063f7a75419
joao <joao.nilton@gmail.com>
2021-02-04 10:41:43 -0300

    ver_1_040221

---
+++ digitalinovationone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace digitalinovationone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

3399de6356eb408edfc647f7edf7cb51466334fe
joao <joao.nilton@gmail.com>
2021-02-09 19:11:25 -0300

    ver_2_090221

--- dino-game/script.js
+++ dino-game/script.js
@@ -81,3 +81,4 @@ document.addEventListener('keyup', handleKeyUp);
 /* Cuidado com a ortografia  05-02-2021*/
 /* Apocalipse now  06-02-2021*/
 /* Not now Apocalipse now  07-02-2021*/
+/* Not now  09-02-2021*/
